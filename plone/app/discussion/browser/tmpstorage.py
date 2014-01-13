3f4174d79604721bcbb678c9c6976a0d6422c5a9
tisto <tisto@plone.org>
2014-01-13 08:13:18 +0100

    Add comments.

--- plone/app/discussion/browser/tmpstorage.py
+++ plone/app/discussion/browser/tmpstorage.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from Products.Five.browser import BrowserView
 from plone.app.discussion.interfaces import IConversation
+from zope.annotation.interfaces import IAnnotations
 from zope.component.hooks import getSite
 
 
@@ -8,22 +9,24 @@ ANNOTATION_KEY = 'plone.app.discussion:conversation'
 
 
 def push_to_tmpstorage(obj):
-    from zope.annotation.interfaces import IAnnotations
     portal = getSite()
+    # Copy the conversation from the object to portal (tmpstorage)
     IConversation(obj).__parent__ = portal
     annotations = IAnnotations(portal)
     annotations[ANNOTATION_KEY] = IConversation(obj)
-    #
+    # Delete the conversation from the object.
+    # XXX: I'm not sure if this is the behaviour we want, we might want to
+    # remove those lines.
     source_ann = IAnnotations(obj)
     del source_ann[ANNOTATION_KEY]
 
 
 def pop_from_tmpstorage(obj):
-    from zope.annotation.interfaces import IAnnotations
+    # Copy the conversation from the portal (tmpstorage) to the object
     annotations = IAnnotations(obj)
     annotations[ANNOTATION_KEY] = IConversation(getSite())
     IConversation(obj).__parent__ = obj
-
+    # Delete the conversation on the portal (tmpstorage)
     portal_ann = IAnnotations(getSite())
     del portal_ann[ANNOTATION_KEY]
 
